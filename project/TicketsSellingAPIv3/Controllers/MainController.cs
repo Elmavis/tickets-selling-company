7a0c752e53619e9c2c227d5fcfe2ee366f4a5f49
Tarasenka Alexander <elmaviss@gmail.com>
2020-12-26 15:15:15 +0300

    added API project

---
+++ project/TicketsSellingAPIv3/Controllers/MainController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Text.Json;
+using TicketsSellingAPIv3.Models;
+
+namespace TicketsSellingAPIv3.Controllers
+{
+    [ApiController]
+    public class MainController : Controller
+    {
+        IRouteRepository _routeRepository;
+        private Cart[] carts = new Cart[10000];
+        private int maxUserId = 1;
+
+
+        public MainController(IRouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository; // добавить в DI
+        }
+
+
+        [HttpGet]
+        public string AddToCart(int routeId, int userId)
+        {
+            RouteType route = _routeRepository.GetAll().FirstOrDefault(repository => repository.Id == routeId);
+
+            if (route != null)
+            {
+                carts[userId].AddItem(route, 1);
+            }
+
+            return "OK";
+        }
+
+        [HttpGet]
+        public string RemoveFromCart(int routeId, int userId)
+        {
+            RouteType route = _routeRepository.GetAll().FirstOrDefault(repository => repository.Id == routeId);
+
+            if (route != null)
+            {
+                carts[userId].RemoveItem(route);
+            }
+            return "OK";
+        }
+
+        [HttpGet]
+        public string GetRoutesByNamesAndDate(string destinationName, string departureName,
+            string departureDate)
+        {
+            DateTime departureDateTime = DateTime.Parse(departureDate);
+            var list = _routeRepository.GetRoutesByNamesAndDate(departureName, destinationName,
+                departureDateTime);
+
+            return JsonSerializer.Serialize(list);
+        }
+
+        [HttpGet]
+        public string GetRoutsOfCart(int userId)
+        {
+            var cartItems = carts[userId].GetItems();
+            return JsonSerializer.Serialize(cartItems);
+        }
+        
+        [HttpGet]
+        public int GetId()
+        {
+            return maxUserId++;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ project/TicketsSellingAPIv3/Models/Cart.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicketsSellingAPIv3.Models
+{
+    [Serializable]
+    public class Cart
+    {
+        private List<CartLine> routesInCart = new List<CartLine>();
+
+        public void AddItem(RouteType route, int quantity)
+        {
+            string routeToString = (route.DepartureName + "-" + route.DestinationName);
+
+            CartLine routeLine = routesInCart
+                .Where(r => r.Route == routeToString)
+                .FirstOrDefault();
+
+            if (routeLine == null)
+            {
+                routesInCart.Add(new CartLine
+                {
+                    Id = route.Id,
+                    Quantity = quantity,
+                    Route = routeToString,
+                    Price = route.Price,
+                    TotalPrice = route.Price * quantity
+                }); ;
+            }
+            else
+            {
+                routeLine.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(RouteType route)
+        {
+            routesInCart.RemoveAll(r => r.Id == route.Id);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return routesInCart.Sum(e => e.Price = e.Quantity);
+        }
+
+        public void Clear()
+        {
+            routesInCart.Clear();
+        }
+
+        public IEnumerable<CartLine> GetItems()
+        {
+            return routesInCart;
+        }
+    }
+
+    [Serializable]
+    public class CartLine
+    {
+        [NonSerialized]
+        public int Id;
+
+        public int Quantity;
+
+        public string Route;
+
+        public decimal Price;
+
+        public decimal TotalPrice;
+    }
+}
